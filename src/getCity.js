918050a5e6f73e7d5fe02707c5cb7b6bdbf1ec6e
tajwararik <tajwar.arik.10@gmail.com>
2024-10-27 18:56:16 +0600

    Added form validation

--- src/getCity.js
+++ src/getCity.js
@@ -1,5 +1,16 @@
 const cityName = document.getElementById("city");
 
+cityName.addEventListener("input", () => {
+  cityName.setCustomValidity("");
+});
+
 export function getCityName() {
-  const city = cityName.value;
+  const pattern = /^[A-Za-z]+$/;
+
+  if (cityName.value === "" || !pattern.test(cityName.value)) {
+    cityName.setCustomValidity("Please enter a valid city name.");
+    cityName.reportValidity();
+  } else {
+    const city = cityName.value;
+  }
 }
