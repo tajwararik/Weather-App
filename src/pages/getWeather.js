1584e4f5345812e0aa8c553b529bbdc0070894ff
tajwararik <tajwar.arik.10@gmail.com>
2024-10-27 21:40:20 +0600

    Fetched weather data

--- src/pages/getWeather.js
+++ src/pages/getWeather.js
@@ -1,4 +1,18 @@
 export function weather(cityName) {
   const city = cityName;
-  console.log(city);
+
+  fetchData(city);
+}
+
+async function fetchData(cityName) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=SXBT5JT6F5USTW2XYTH4YNYCD`,
+      { mode: "cors" }
+    );
+    const getData = await response.json();
+    console.log(getData);
+  } catch (err) {
+    alert(err);
+  }
 }
