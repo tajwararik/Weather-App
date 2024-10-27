a13f076f0af5959ff72b07afdf75d33678bf0bc1
tajwararik <tajwar.arik.10@gmail.com>
2024-10-27 19:02:19 +0600

    Created pages folder

---
+++ src/pages/getCity.js
@@ -0,0 +1,16 @@
+const cityName = document.getElementById("city");
+
+cityName.addEventListener("input", () => {
+  cityName.setCustomValidity("");
+});
+
+export function getCityName() {
+  const pattern = /^[A-Za-z]+$/;
+
+  if (cityName.value === "" || !pattern.test(cityName.value)) {
+    cityName.setCustomValidity("Please enter a valid city name.");
+    cityName.reportValidity();
+  } else {
+    const city = cityName.value;
+  }
+}
