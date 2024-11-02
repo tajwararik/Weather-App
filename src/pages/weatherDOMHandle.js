3da79ff94268f7c24ad13ea9e2f8008bf3a2bcb2
tajwararik <tajwar.arik.10@gmail.com>
2024-11-02 20:35:12 +0600

    Added functionality to convert to celsius

--- src/pages/weatherDOMHandle.js
+++ src/pages/weatherDOMHandle.js
@@ -7,10 +7,26 @@ export function getInformation(city, temp, feelsTemp, humidity, description) {
   section.innerHTML = `<h1>${city}</h1>
         <button class="celsius-btn"><span>&deg</span>C</button>
         <button class="fahrenheit-btn"><span>&deg</span>F</button>
-        <p>${temp}<span>&deg</span>F</p>
-        <p>Feels like <span>${feelsTemp}</span><span>&deg</span>F</p>
+        <p class = "temp">${temp}<span>&deg</span>F</p>
+        <p class = feels-temp>Feels like ${feelsTemp}<span>&deg</span>F</p>
         <p>Humidity <span>${humidity}%</span></p>
         <p>${description}</p>`;
 
   body.append(section);
+
+  section.querySelector(".celsius-btn").addEventListener("click", () => {
+    const fahrenheit = temp;
+    const feelsFahrenheit = feelsTemp;
+
+    const celsius = ((fahrenheit - 32) / 9) * 5;
+    const feelCelsius = ((feelsFahrenheit - 32) / 9) * 5;
+
+    section.querySelector(".temp").innerHTML = `${celsius.toFixed(
+      1
+    )}<span>&deg</span>C`;
+
+    section.querySelector(
+      ".feels-temp"
+    ).innerHTML = `Feels like ${feelCelsius.toFixed(1)}<span>&deg</span>C`;
+  }); 
 }
