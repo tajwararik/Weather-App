4db574da7c12c1a349e2a75736effae70ca71582
tajwararik <tajwar.arik.10@gmail.com>
2024-11-02 19:11:52 +0600

    Added celsius and fahrenheit button to section

--- src/pages/weatherDOMHandle.js
+++ src/pages/weatherDOMHandle.js
@@ -1,16 +1,12 @@
 const body = document.querySelector("body");
 export const section = document.createElement("section");
 
-export function getInformation(
-  city,
-  temp,
-  feelsTemp,
-  humidity,
-  description
-) {
+export function getInformation(city, temp, feelsTemp, humidity, description) {
   section.innerHTML = "";
 
   section.innerHTML = `<h1>${city}</h1>
+        <button class="celsius-btn"><span>&deg</span>C</button>
+        <button class="fahrenheit-btn"><span>&deg</span>F</button>
         <p>${temp}<span>&deg</span>F</p>
         <p>Feels like <span>${feelsTemp}</span><span>&deg</span>F</p>
         <p>Humidity <span>${humidity}%</span></p>
